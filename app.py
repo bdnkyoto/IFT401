e076c8a77c4e23a472f9c38dba9b2fb1a319f232
WaffleChomper <92765026+WaffleChomper@users.noreply.github.com>
2025-02-18 18:40:52 -0700

    Created file structure with app.py and templates
    File structure, app.py, and templates were created. Base.html was brought over from one of our earlier assignments. Reach out to me for questions.

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5 
+
+app = Flask(__name__)
+
+bootstrap = Bootstrap5(app)
+
+@app.route('/') 
+
+def home():
+    return render_template('portfolio.html')
+
+@app.route('/portfolio') 
+
+def portfolio():
+    return render_template('portfolio.html') 
+
+
+@app.route('/trade') 
+
+def trade():
+    return render_template('trade.html') 
+
+if __name__ == '__main__':    
+    app.run(debug=True)
+
+     #.\venv\Scripts\activate To Start Virtual
+    #flask --app app run --debug To run in debug mode
+
+    # python -m venv venv to create virtual environment
